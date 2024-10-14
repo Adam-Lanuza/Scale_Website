fb867323b571be70db00ae7be350b604f81afd67
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2024-10-14 21:24:57 +0800

    Updated pdo.php
    Added a new function getSQLData() which will take the procedure name input, query the data, and return an object form of the data.

--- pdo.php
+++ pdo.php
@@ -1,6 +1,21 @@
 <!--Connects the php to mysql-->
 <?php
 $pdo = new PDO("mysql:host=localhost;port=3306;dbname=scalesite", "admin", "scaleable");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);?>
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+function getSQLData($procedure) {
+	global $pdo;
+	$sql = $pdo->query("CALL $procedure");
+	$sqldatatemp = [];
+
+	while ($inforow = $sql->fetch(PDO::FETCH_OBJ)) {
+		array_push($sqldatatemp, get_object_vars($inforow));
+	}
+
+	$sql ->closeCursor();
+	return $sqldatatemp;
+}
+
+?>
 
 <!--If you want, you can change "admin" and "scaleable" into any username and password you want. Just make sure that they are the same values as the GRANT statement-->
