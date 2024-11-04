0bb166f1b3d1923849dcd0180c9886ad17ceae41
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2024-11-05 02:12:21 +0800

    Added new procedures

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -1,35 +1,46 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Add_Question`(IN `question` VARCHAR(255), IN `category` VARCHAR(32), IN `answer` TEXT)
-	INSERT INTO `scalefaq` (`question`, `category`, `answer`)
-	VALUES (question, category, answer)$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Add_Adult_Supervisor`(IN `pid` INT, IN `aid` INT, IN `pos` VARCHAR(128), IN `ib` INT)
+INSERT INTO `adultsupervisors`(`personid`, `activityid`, `position`, `insertedby`)
+VALUES (pid, aid, pos, ib)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Add_Question`(IN `question` VARCHAR(255), IN `category` VARCHAR(32), IN `answer` TEXT, IN `insertedby` INT)
+INSERT INTO `scalefaq` (`question`, `category`, `answer`, `insertedby`)
+	VALUES (question, category, answer, insertedby)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Add_Student_to_Activity`(IN `sid` INT, IN `aid` INT, IN `p` VARCHAR(64), IN `s` VARCHAR(16), IN `ib` INT)
+INSERT INTO `activitystudents`(`studentid`, `activityid`, `position`, `status`, `insertedby`)
+VALUES (sid, aid, p, s, ib)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Approve_Activity`(IN `aid` INT)
+UPDATE `activities`
+SET `approved`=TRUE, `approvaldate`=CURRENT_TIMESTAMP, `overallstatus`='IP-P'
+WHERE `activityid` = aid$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Create_Activity`(IN `ti` VARCHAR(255), IN `ac` VARCHAR(8), IN `ty` VARCHAR(16), IN `psd` DATE, IN `ped` DATE, IN `isd` DATE, IN `ied` DATE, IN `v` TEXT, IN `d` TEXT, IN `o` TEXT, IN `p` VARCHAR(16), IN `ib` INT)
+INSERT INTO `activities`
+	(`name`, `activitycode`, `type`, `prepstartdate`, `prependdate`, `implementstartdate`, `implementenddate`, `venue`, `description`, `objectives`, `publicity`, `overallstatus`, `insertedby`)
+VALUES
+	(ti, ac, ty, psd, ped, isd, ied, v, d, o, p, 'P', ib)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Delete_Question`(IN `qid` INT)
-	UPDATE `scalefaq`
+UPDATE `scalefaq`
 	SET `scalefaq`.`isactive` = FALSE
 	WHERE `scalefaq`.`scalefaqid` = qid$$
-    
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Edit_Question`(IN `qid` INT, IN `question` VARCHAR(255), IN `category` VARCHAR(32), IN `answer` TEXT)
-	UPDATE `scalefaq`
+UPDATE `scalefaq`
 	SET `scalefaq`.`question` = question,
 		`scalefaq`.`category` = category,
 		`scalefaq`.`answer` = answer
 	WHERE `scalefaq`.`scalefaqid` = qid$$
-    
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Activity_LOs`(IN `activityid` INT, IN `studentid` INT)
-	SELECT
-		`studentscalereqs`.`studentscalereqid` AS `studentscalereqs`,
-		`studentscalereqs`.`activitystudentid` AS `activitystudentid`,
-		`activitystudents`.`studentid` AS `studentid`,
+SELECT DISTINCT
 		`activitystudents`.`activityid` AS `activityid`,
 		`studentscalereqs`.`scalerequirementid` AS `scalerequirementid`,
 		`scalerequirements`.`title` AS `scalereqtitle`,
 		`scalerequirements`.`description` AS `scalereqdescription`,
 		`scalerequirements`.`shortname` AS `scalereqshortname`,
-		`studentscalereqs`.`approved` AS `studentscaleapproved`,
-		`studentscalereqs`.`approvaldate` AS `studentscaleapprovaldate`,
-		`studentscalereqs`.`completed` AS `studentscalecompleted`,
-		`studentscalereqs`.`completiondate` AS `studentscalecompletiondate`,
-		`studentscalereqs`.`scaleadvisorid` AS `scaleadvisorid`,
 		`studentscalereqs`.`isactive` AS `isactive`
 	FROM `studentscalereqs`
 	JOIN `activitystudents` ON `activitystudents`.`activitystudentid` = `studentscalereqs`.`activitystudentid`
@@ -37,23 +48,17 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Activity_LOs`(IN `activityid` I
 	WHERE `activitystudents`.`activityid` = activityid
 		AND (`activitystudents`.`studentid` = studentid OR studentid = 0)
 		AND `studentscalereqs`.`scalerequirementid` IN (5, 6, 7, 8, 9, 10, 11, 12)
+        AND ((`activitystudents`.`status` = 'P') OR
+        	(`studentscalereqs`.`approved`))
 		AND `studentscalereqs`.`isactive`$$
-        
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Activity_Strands`(IN `activityid` INT, IN `studentid` INT)
-	SELECT
-		`studentscalereqs`.`studentscalereqid` AS `studentscalereqs`,
-		`studentscalereqs`.`activitystudentid` AS `activitystudentid`,
-		`activitystudents`.`studentid` AS `studentid`,
+SELECT DISTINCT
 		`activitystudents`.`activityid` AS `activityid`,
 		`studentscalereqs`.`scalerequirementid` AS `scalerequirementid`,
 		`scalerequirements`.`title` AS `scalereqtitle`,
 		`scalerequirements`.`description` AS `scalereqdescription`,
 		`scalerequirements`.`shortname` AS `scalereqshortname`,
-		`studentscalereqs`.`approved` AS `studentscaleapproved`,
-		`studentscalereqs`.`approvaldate` AS `studentscaleapprovaldate`,
-		`studentscalereqs`.`completed` AS `studentscalecompleted`,
-		`studentscalereqs`.`completiondate` AS `studentscalecompletiondate`,
-		`studentscalereqs`.`scaleadvisorid` AS `scaleadvisorid`,
 		`studentscalereqs`.`isactive` AS `isactive`
 	FROM `studentscalereqs`
 	JOIN `activitystudents` ON `activitystudents`.`activitystudentid` = `studentscalereqs`.`activitystudentid`
@@ -61,38 +66,64 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Activity_Strands`(IN `activityi
 	WHERE `activitystudents`.`activityid` = activityid
 		AND (`activitystudents`.`studentid` = studentid OR studentid = 0)
 		AND `studentscalereqs`.`scalerequirementid` IN (1, 2, 3, 4)
+        AND ((`activitystudents`.`status` = 'P') OR
+        	(`studentscalereqs`.`approved`))
 		AND `studentscalereqs`.`isactive`$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_All_Questions`()
-	SELECT * FROM `scalefaq`
+SELECT * FROM `scalefaq`
 	WHERE `scalefaq`.`isactive` = TRUE
 	ORDER BY `scalefaq`.`category`$$
-    
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Question_Categories`()
-	SELECT DISTINCT `scalefaq`.`category` FROM `scalefaq`
+SELECT DISTINCT `scalefaq`.`category` FROM `scalefaq`
 	WHERE `scalefaq`.`isactive` = TRUE
 	ORDER BY `category`$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Student_Activities`(IN `studentid` INT)
-	SELECT
-		`activitystudents`.`activitystudentid` AS `activitystudentid`,
-		`activitystudents`.`studentid` AS `studentid`,
-		`activitystudents`.`activityid` AS `activityid`,
-		`activities`.`name` AS `activityname`,
-		`activities`.`prepstartdate` AS `prepstartdate`,
-		`activities`.`prependdate` AS `prependdate`,
-		`activities`.`implementstartdate` AS `implementstartdate`,
-		`activities`.`implementenddate` AS `implementenddate`,
-		`activities`.`venue` AS `activityvenue`,
-		`activities`.`description` AS `activitydescription`,
-		`activities`.`objectives` AS `activityobjectives`,
-		`activities`.`approved` AS `activityapproved`,
-		`activities`.`approvaldate` AS `activityapprovaldate`,
-		`activitystudents`.`position` AS `activityposition`,
-		`activitystudents`.`status` AS `activitystatus`,
-		`activitystudents`.`isactive` AS `isactive`
-	FROM `activitystudents`
-	JOIN `activities` ON `activities`.`activityid` = `activitystudents`.`activityid`
-	WHERE `activitystudents`.`studentid` = studentid
-		AND activitystudents.`isactive` = TRUE$$
+SELECT
+	`activitystudents`.`activitystudentid` AS `activitystudentid`,
+	`activitystudents`.`studentid` AS `studentid`,
+	`activitystudents`.`activityid` AS `activityid`,
+	`activities`.`name` AS `activityname`,
+	`activities`.`prepstartdate` AS `prepstartdate`,
+	`activities`.`prependdate` AS `prependdate`,
+	`activities`.`implementstartdate` AS `implementstartdate`,
+	`activities`.`implementenddate` AS `implementenddate`,
+	`activities`.`venue` AS `activityvenue`,
+	`activities`.`description` AS `activitydescription`,
+	`activities`.`objectives` AS `activityobjectives`,
+	`activities`.`approved` AS `activityapproved`,
+	`activities`.`approvaldate` AS `activityapprovaldate`,
+	`activitystudents`.`position` AS `activityposition`,
+	`activitystudents`.`status` AS `activitystatus`,
+	`activitystudents`.`isactive` AS `isactive`
+FROM `activitystudents`
+JOIN `activities` ON `activities`.`activityid` = `activitystudents`.`activityid`
+WHERE `activitystudents`.`studentid` = studentid
+	AND activitystudents.`isactive` = TRUE
+ORDER BY `activitystudents`.`status` DESC$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Supervisor_Activities`(IN `personid` INT)
+SELECT
+	`adultsupervisors`.`supervisorid` AS `supervisorid`,
+    `adultsupervisors`.`activityid` AS `activityid`,
+	`activities`.`name` AS `activityname`,
+	`activities`.`prepstartdate` AS `prepstartdate`,
+	`activities`.`prependdate` AS `prependdate`,
+	`activities`.`implementstartdate` AS `implementstartdate`,
+	`activities`.`implementenddate` AS `implementenddate`,
+	`activities`.`venue` AS `activityvenue`,
+	`activities`.`description` AS `activitydescription`,
+	`activities`.`objectives` AS `activityobjectives`,
+	`activities`.`approved` AS `activityapproved`,
+	`activities`.`approvaldate` AS `activityapprovaldate`,
+    `adultsupervisors`.`position` AS `activityposition`,
+    `activities`.`overallstatus` AS `activitystatus`,
+	`adultsupervisors`.`isactive` AS `isactive`
+FROM `adultsupervisors`
+JOIN `activities` ON `activities`.`activityid` = `adultsupervisors`.`activityid`
+WHERE `adultsupervisors`.`personid` = personid
+	AND `adultsupervisors`.`isactive` = TRUE
+ORDER BY `activities`.`overallstatus` DESC$$
 DELIMITER ;
